851b406c707ba228e5b4b52b3796e3d423f4f140
Quinne Farenwald <q.farenwald@gmail.com>
2019-07-25 22:33:00 -0600

    Add basic set up code to files

---
+++ todo-list.js
@@ -0,0 +1,25 @@
+class ToDoList {
+  constructor(title) {
+    this.title = title;
+    this.tasks = [];
+    this.id = dateTime();
+    // how write above?
+    this.urgent = false;
+  }
+
+  saveToStorage() {
+
+  }
+
+  deleteFromStorage() {
+
+  }
+
+  updateToDo() {
+
+  }
+
+  updateTask() {
+
+  }
+}
